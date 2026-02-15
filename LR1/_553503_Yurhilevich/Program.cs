a19720610590298aca74cefb38a5c42d5e456fd9
Юргилевич Валерия <lerayurhilevich@icloud.com>
2026-02-15 23:32:47 +0300

    Init with LR1 and LR2

---
+++ LR1/_553503_Yurhilevich/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main()
+        {
+            double first_number;
+            double second_number;
+
+            Console.WriteLine("Введите делимое: ");
+            string? input1 = Console.ReadLine();
+
+            bool isNumber = double.TryParse(input1, out first_number);
+
+            while (isNumber == false)
+            {
+                Console.WriteLine("Ошибка! Введено некорректное значение.");
+                Console.WriteLine("Введите заново делимое: ");
+                input1 = Console.ReadLine();
+
+                isNumber = double.TryParse(input1, out first_number);
+            }
+
+            Console.WriteLine($"Делимое = {first_number}");
+
+            Console.WriteLine("Введите делитель: ");
+            string? input2 = Console.ReadLine();
+
+            isNumber = double.TryParse(input2, out second_number);
+
+            while (isNumber == false || Convert.ToDouble(input2) == 0)
+            {
+                Console.WriteLine("Ошибка! Введено некорректное значение.");
+                Console.WriteLine("Введите заново делитель: ");
+                input2 = Console.ReadLine();
+
+                isNumber = double.TryParse(input2, out second_number);
+            }
+
+            Console.WriteLine($"Делитель = {first_number}");
+
+            double result = first_number / second_number;
+            Console.WriteLine($"Результат деление равен {result}.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LR2/task1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel;
+using System.Security.Principal;
+
+namespace t1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Вариант 14\nЗадание: Определить, какая из цифр трехзначного числа больше: вторая или последняя");
+
+
+            while (true)
+            {
+                int number;
+                Console.WriteLine("Введите трёхзначное число: ");
+                string? input1 = Console.ReadLine();
+
+                bool isNumber = int.TryParse(input1, out number);
+
+                while (isNumber == false || input1.Length > 3 || input1.Length < 3)
+                {
+                    Console.WriteLine("Ошибка! Введено некорректное значение.\nВведите заново трёхзначное число: ");
+                    input1 = Console.ReadLine();
+
+                    isNumber = int.TryParse(input1, out number);
+                }
+                if (isNumber == true && input1.Length == 3)
+                {
+                    int mod = number % 100;
+                    int second_number = mod/10;
+                    int third_number = mod%10;
+
+                    if (second_number > third_number) Console.WriteLine("Второе число больше последнего");
+                    else if (second_number < third_number) Console.WriteLine("Последнее число больше второго");
+                    else Console.WriteLine("Второе и последнее число одинаковы");
+                }
+
+                int answer;
+                Console.WriteLine("Хотите ли продолжить?(1-да; 2-нет): ");
+                string? input2 = Console.ReadLine();
+                isNumber = int.TryParse(input2, out answer);
+
+                while (isNumber == false || (answer != 1 && answer != 2))
+                {
+                    Console.WriteLine("Ошибка! Введено некорректное значение.\nВведите заново ответ\nХотите ли продолжить?(1-да; 2-нет): ");
+                    input2 = Console.ReadLine();
+
+                    isNumber = int.TryParse(input2, out answer);
+                }
+
+                switch (answer)
+                {
+                    case 1:
+                        continue;
+                    case 2:
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+    }
+}
